ae27c24e76dc4e0d5b313be7d9c8b21387489a27
mskreczko <mic.skreczko@gmail.com>
2023-03-12 09:59:51 +0100

    implemented jwt authorization

---
+++ api/src/main/java/pl/mskreczko/api/config/jwt/AccessTokenProvider.java
@@ -0,0 +1,11 @@
+package pl.mskreczko.api.config.jwt;
+
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AccessTokenProvider {
+    String getAccessToken(UUID userId, List<String> roles);
+    Authentication extractAuthentication(String token);
+}
