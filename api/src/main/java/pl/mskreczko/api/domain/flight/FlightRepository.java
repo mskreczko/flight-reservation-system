25caf75168e646bdfb343b093719ea6ad8efb1bc
mskreczko <mic.skreczko@gmail.com>
2023-03-29 18:42:00 +0200

    fetching flights based on criteria

--- api/src/main/java/pl/mskreczko/api/domain/flight/FlightRepository.java
+++ api/src/main/java/pl/mskreczko/api/domain/flight/FlightRepository.java
@@ -1,10 +1,15 @@
 package pl.mskreczko.api.domain.flight;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import pl.mskreczko.api.domain.airport.Airport;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FlightRepository extends PagingAndSortingRepository<Flight, UUID> {
     Optional<Flight> findById(UUID id);
+    Page<Flight> findByDepartureAirportAndDestinationAirport(Airport departureAirport, Airport destinationAirport,
+                                                             PageRequest pageRequest);
 }
